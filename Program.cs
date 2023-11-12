1473de751ce262b452a86af7aa6fcc230895e68f
CaduuuS2 <carloseduardod346@gmail.com>
2023-11-12 00:25:06 -0300

    Desenvolvimento da relação de herança entre iphone e nokia com smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+    Smartphone nokia = new Nokia("22258190", "s23", "1234", 2024);
+    Smartphone iphone = new Iphone("22253241", "i15", "1254", 2024);
+
+    nokia.InstalarAplicativo("WhatsApp");
+    iphone.InstalarAplicativo("Telegram");
+
+    nokia.Ligar();
+    iphone.Ligar();
+
+    nokia.ReceberLigacao();
+    iphone.ReceberLigacao();
